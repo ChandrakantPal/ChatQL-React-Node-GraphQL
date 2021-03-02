6be863ed7e8ab460b94bdfc0c5771543d6464769
ChandrakantPal <sanjay161097@gmail.com>
2021-03-02 23:01:28 +0530

    create user

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -10,4 +10,30 @@ module.exports = {
       }
     },
   },
+  Mutation: {
+    register: async (_, args) => {
+      const { username, email, password, confirmPassword } = args
+
+      try {
+        // validate input data
+
+        // check if username / email exist
+
+        // Hash password
+
+        // create user
+        const user = await User.create({
+          username,
+          email,
+          password,
+        })
+
+        // return user
+        return user
+      } catch (err) {
+        console.log(err)
+        throw err
+      }
+    },
+  },
 }
