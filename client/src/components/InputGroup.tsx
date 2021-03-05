388be581a2ceec13bdef18620f20862161f5595e
ChandrakantPal <sanjay161097@gmail.com>
2021-03-05 23:35:55 +0530

    messanger style component

---
+++ client/src/components/InputGroup.tsx
@@ -0,0 +1,27 @@
+interface InputGroupProps {
+  type: string
+  placeholder: string
+  value: string
+  setValue: (str: string) => void
+}
+
+const InputGroup: React.FC<InputGroupProps> = ({
+  placeholder,
+  setValue,
+  type,
+  value,
+}) => {
+  return (
+    <div className="mb-2">
+      <input
+        type={type}
+        className="p-3 transition duration-200 border border-gray-300 rounded-full outline-none bg-gray-50 focus:bg-white hover:bg-white focus:border-blue-500"
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </div>
+  )
+}
+
+export default InputGroup
