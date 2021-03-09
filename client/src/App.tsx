da3266333bf5410d94e0ceca999fa76378acd984
ChandrakantPal <sanjay161097@gmail.com>
2021-03-09 23:53:06 +0530

    react router setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import ApolloProvider from './ApolloProvider'
 
+import Login from './components/Login'
 import Register from './components/Register'
 
 function App() {
   return (
     <ApolloProvider>
-      <Register />
+      <BrowserRouter>
+        <div className="container mx-auto">
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </ApolloProvider>
   )
 }
