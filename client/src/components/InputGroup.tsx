42db3d07a71c314e5527395af2d932d2ab227d9a
ChandrakantPal <sanjay161097@gmail.com>
2021-03-09 23:29:51 +0530

    errors showed below input

--- client/src/components/InputGroup.tsx
+++ client/src/components/InputGroup.tsx
@@ -1,8 +1,10 @@
+import classNames from 'classnames'
 interface InputGroupProps {
   type: string
   placeholder: string
   value: string
   setValue: (str: string) => void
+  error: string | undefined
 }
 
 const InputGroup: React.FC<InputGroupProps> = ({
@@ -10,16 +12,23 @@ const InputGroup: React.FC<InputGroupProps> = ({
   setValue,
   type,
   value,
+  error,
 }) => {
   return (
     <div className="mb-3">
       <input
         type={type}
-        className="p-3 transition duration-200 border border-gray-300 rounded-full outline-none bg-gray-50 focus:bg-white hover:bg-white focus:border-blue-500"
+        className={classNames(
+          'p-3 transition duration-200 border border-gray-300 rounded-full outline-none bg-gray-50 focus:bg-white hover:bg-white focus:border-blue-500',
+          { 'border-red-500': error }
+        )}
         placeholder={placeholder}
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
+      <small className="block font-medium text-center text-red-600">
+        {error}
+      </small>
     </div>
   )
 }
