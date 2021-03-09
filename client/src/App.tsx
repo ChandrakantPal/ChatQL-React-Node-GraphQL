09327807d22afe07bc45bac4355b11fb4c158269
ChandrakantPal <sanjay161097@gmail.com>
2021-03-10 00:13:35 +0530

    home route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import ApolloProvider from './ApolloProvider'
+import Home from './components/Home'
 
 import Login from './components/Login'
 import Register from './components/Register'
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <div className="container mx-auto">
           <Switch>
+            <Route exact path="/" component={Home} />
             <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
           </Switch>
