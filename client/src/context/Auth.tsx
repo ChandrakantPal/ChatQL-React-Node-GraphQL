e786f964dfe3b47e41179ca5a6290921287374f6
ChandrakantPal <sanjay161097@gmail.com>
2021-03-25 23:52:28 +0530

    jwt decode to check authentication on reload

--- client/src/context/Auth.tsx
+++ client/src/context/Auth.tsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useReducer } from 'react'
+import jwt_decode, { JwtPayload } from 'jwt-decode'
 import { User } from '../types'
 
 interface State {
@@ -10,6 +11,24 @@ interface Action {
   type: string
   payload: any
 }
+
+const initialState: State = {
+  user: null,
+  authenticated: false,
+}
+const token = localStorage.getItem('token')
+if (token) {
+  const decodedToken = jwt_decode<JwtPayload>(token)
+  const expiresAt = new Date(decodedToken.exp * 1000)
+  if (new Date() > expiresAt) {
+    localStorage.removeItem('token')
+  } else {
+    initialState.user = decodedToken as User
+    initialState.authenticated = true
+  }
+  console.log({ decodedToken })
+  console.log({ expiresAt })
+} else console.log('no token found')
 
 const StateContext = createContext<State>({
   user: null,
@@ -34,27 +53,24 @@ const authReducer = (state: State, { type, payload }: Action) => {
         authenticated: false,
         user: null,
       }
-    case 'AUTH_CHECK':
-      return {
-        ...state,
-        authenticated: true,
-      }
+    // case 'AUTH_CHECK':
+    //   return {
+    //     ...state,
+    //     authenticated: true,
+    //   }
     default:
       throw new Error(`Unknow action type: ${type}`)
   }
 }
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [state, defaultDispatch] = useReducer(authReducer, {
-    user: null,
-    authenticated: false,
-  })
+  const [state, defaultDispatch] = useReducer(authReducer, initialState)
 
-  useEffect(() => {
-    if (localStorage.getItem('token')) {
-      dispatch('AUTH_CHECK')
-    }
-  }, [])
+  // useEffect(() => {
+  //   if (localStorage.getItem('token')) {
+  //     dispatch('AUTH_CHECK')
+  //   }
+  // }, [])
 
   const dispatch = (type: string, payload?: any) =>
     defaultDispatch({ type, payload })
