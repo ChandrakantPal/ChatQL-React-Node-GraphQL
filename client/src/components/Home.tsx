9c89cb87bf991ab6b0e66893be58706f4ca73024
ChandrakantPal <sanjay161097@gmail.com>
2021-03-26 21:40:36 +0530

    users displayed

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Redirect } from 'react-router'
 import { useAuthState } from '../context/Auth'
 import Navbar from './Navbar'
+import { User } from '../types'
 
 const Home = () => {
   const { authenticated } = useAuthState()
@@ -13,14 +14,31 @@ const Home = () => {
     console.log(error)
   }
 
-  if (data) {
-    console.log(data)
+  let usersMarkup
+  if (!data || loading) {
+    usersMarkup = <p>Loading...</p>
+  } else if (data.getUsers.length === 0) {
+    usersMarkup = <p>No users have joined yet</p>
+  } else if (data.getUsers.length > 0) {
+    usersMarkup = data.getUsers.map((user: User) => (
+      <div key={user.username}>
+        <p>{user.username}</p>
+      </div>
+    ))
   }
 
   if (!authenticated) return <Redirect to="/login" />
   return (
     <div className="w-full h-full">
       <Navbar />
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3">
+          <div>{usersMarkup}</div>
+          <div className="col-span-2">
+            <p>Messages</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
