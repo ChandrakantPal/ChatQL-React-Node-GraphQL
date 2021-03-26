b13ddd3cf709b371f6bf3e42dbfb6a43f1a05dc7
ChandrakantPal <sanjay161097@gmail.com>
2021-03-26 21:05:43 +0530

    redirect to login in not authenticated

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Redirect } from 'react-router'
+import { useAuthState } from '../context/Auth'
 import Navbar from './Navbar'
 
 const Home = () => {
+  const { authenticated } = useAuthState()
+  if (!authenticated) return <Redirect to="/login" />
   return (
     <div className="w-full h-full">
       <Navbar />
