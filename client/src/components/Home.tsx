400fa5670cfce03156a8486c2f2cf6a7a0825bb3
ChandrakantPal <sanjay161097@gmail.com>
2021-03-26 21:23:45 +0530

    querying users

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,3 +1,4 @@
+import { gql, useQuery } from '@apollo/client'
 import React from 'react'
 import { Redirect } from 'react-router'
 import { useAuthState } from '../context/Auth'
@@ -5,6 +6,17 @@ import Navbar from './Navbar'
 
 const Home = () => {
   const { authenticated } = useAuthState()
+
+  const { loading, data, error } = useQuery(GET_USERS)
+
+  if (error) {
+    console.log(error)
+  }
+
+  if (data) {
+    console.log(data)
+  }
+
   if (!authenticated) return <Redirect to="/login" />
   return (
     <div className="w-full h-full">
@@ -14,3 +26,13 @@ const Home = () => {
 }
 
 export default Home
+
+const GET_USERS = gql`
+  query getUsers {
+    getUsers {
+      username
+      email
+      createdAt
+    }
+  }
+`
