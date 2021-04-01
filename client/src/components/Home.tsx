ea5bd07c79340a6bdf3ae70f1f8e7e979bf4b1e2
ChandrakantPal <sanjay161097@gmail.com>
2021-04-01 22:46:16 +0530

    using users component in home

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -4,12 +4,12 @@ import { Redirect } from 'react-router'
 import { useAuthState } from '../context/Auth'
 import Navbar from './Navbar'
 import { User, Message } from '../types'
+import Users from './Users'
+import Messages from './Messages'
 
 const Home = () => {
   const [selectedUser, setSelectedUser] = useState('')
   const { authenticated } = useAuthState()
-
-  const { loading, data, error } = useQuery(GET_USERS)
 
   const [
     getMessages,
@@ -26,47 +26,15 @@ const Home = () => {
     console.log({ messagesData })
   }
 
-  if (error) {
-    console.log(error)
-  }
-
-  let usersMarkup
-  if (!data || loading) {
-    usersMarkup = <p>Loading...</p>
-  } else if (data.getUsers.length === 0) {
-    usersMarkup = <p>No users have joined yet</p>
-  } else if (data.getUsers.length > 0) {
-    usersMarkup = data.getUsers.map((user: User) => (
-      <div
-        className="flex p-3"
-        key={user.username}
-        onClick={() => setSelectedUser(user.username)}
-      >
-        <img
-          src={user.imageUrl}
-          alt="user"
-          className="object-cover w-16 h-16 mr-2 rounded-full"
-        />
-        <div>
-          <p className="text-green-400">{user.username}</p>
-          <p className="font-light">
-            {user.latestMessage
-              ? user.latestMessage.content
-              : 'You are now connected!'}
-          </p>
-        </div>
-      </div>
-    ))
-  }
-
   if (!authenticated) return <Redirect to="/login" />
   return (
     <div className="w-full h-full">
       <Navbar />
       <div className="container mx-auto">
         <div className="grid grid-cols-3">
-          <div className="bg-gray-100">{usersMarkup}</div>
+          <Users setSelectedUser={setSelectedUser} />
           <div className="col-span-2">
+            <Messages />
             {messagesData && messagesData.getMessages.length > 0 ? (
               messagesData.getMessages.map((message: Message) => (
                 <p key={message.uuid}>{message.content}</p>
