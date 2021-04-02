f9fc6ce9a6a88d2d3ff451da8a5847440d245664
ChandrakantPal <sanjay161097@gmail.com>
2021-04-02 23:05:10 +0530

    message provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,18 +6,21 @@ import Home from './components/Home'
 import Login from './components/Login'
 import Register from './components/Register'
 import { AuthProvider } from './context/Auth'
+import { MessageProvider } from './context/Message'
 
 function App() {
   return (
     <ApolloProvider>
       <AuthProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-          </Switch>
-        </BrowserRouter>
+        <MessageProvider>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+            </Switch>
+          </BrowserRouter>
+        </MessageProvider>
       </AuthProvider>
     </ApolloProvider>
   )
