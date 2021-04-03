5db2ec2921c5d7d0c68042f1578ff06e01836ff6
ChandrakantPal <sanjay161097@gmail.com>
2021-04-03 17:25:07 +0530

    messages component used

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,30 +1,13 @@
-import { gql, useLazyQuery, useQuery } from '@apollo/client'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Redirect } from 'react-router'
 import { useAuthState } from '../context/Auth'
 import Navbar from './Navbar'
-import { User, Message } from '../types'
 import Users from './Users'
 import Messages from './Messages'
 
 const Home = () => {
   const [selectedUser, setSelectedUser] = useState('')
   const { authenticated } = useAuthState()
-
-  const [
-    getMessages,
-    { loading: messagesLoading, data: messagesData },
-  ] = useLazyQuery(GET_MESSAGES)
-
-  useEffect(() => {
-    if (selectedUser) {
-      getMessages({ variables: { from: selectedUser } })
-    }
-  }, [getMessages, selectedUser])
-
-  if (messagesData) {
-    console.log({ messagesData })
-  }
 
   if (!authenticated) return <Redirect to="/login" />
   return (
@@ -36,16 +19,7 @@ const Home = () => {
             setSelectedUser={setSelectedUser}
             selectedUser={selectedUser}
           />
-          <div className="col-span-2">
-            <Messages />
-            {messagesData && messagesData.getMessages.length > 0 ? (
-              messagesData.getMessages.map((message: Message) => (
-                <p key={message.uuid}>{message.content}</p>
-              ))
-            ) : (
-              <p>You are now connected!</p>
-            )}
-          </div>
+          <Messages selectedUser={selectedUser} />
         </div>
       </div>
     </div>
@@ -53,29 +27,3 @@ const Home = () => {
 }
 
 export default Home
-
-const GET_USERS = gql`
-  query getUsers {
-    getUsers {
-      username
-      createdAt
-      imageUrl
-      latestMessage {
-        uuid
-        content
-        from
-        to
-      }
-    }
-  }
-`
-const GET_MESSAGES = gql`
-  query getMessages($from: String!) {
-    getMessages(from: $from) {
-      content
-      from
-      to
-      createdAt
-    }
-  }
-`
