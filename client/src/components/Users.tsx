ae28f5dec2d8a10d1031f16808ee4970b90e5e67
ChandrakantPal <sanjay161097@gmail.com>
2021-04-03 17:16:03 +0530

    selectedUser prop

--- client/src/components/Users.tsx
+++ client/src/components/Users.tsx
@@ -1,12 +1,14 @@
 import { gql, useQuery } from '@apollo/client'
+import { classNames } from '@ungap/global-this'
 import { useMessageDispatch, useMessageState } from '../context/Message'
 import { User } from '../types'
 
 interface UsersProps {
   setSelectedUser: (user: string) => void
+  selectedUser: string
 }
 
-const Users: React.FC<UsersProps> = ({ setSelectedUser }) => {
+const Users: React.FC<UsersProps> = ({ setSelectedUser, selectedUser }) => {
   const dispatch = useMessageDispatch()
   const { users } = useMessageState()
   const { loading } = useQuery(GET_USERS, {
@@ -19,27 +21,32 @@ const Users: React.FC<UsersProps> = ({ setSelectedUser }) => {
   } else if (users.length === 0) {
     usersMarkup = <p>No users have joined yet</p>
   } else if (users.length > 0) {
-    usersMarkup = users.map((user: User) => (
-      <div
-        className="flex p-3 cursor-pointer"
-        key={user.username}
-        onClick={() => setSelectedUser(user.username)}
-      >
-        <img
-          src={user.imageUrl}
-          alt="user"
-          className="object-cover w-16 h-16 mr-2 rounded-full"
-        />
-        <div>
-          <p className="text-green-400">{user.username}</p>
-          <p className="font-light">
-            {user.latestMessage
-              ? user.latestMessage.content
-              : 'You are now connected!'}
-          </p>
+    usersMarkup = users.map((user: User) => {
+      const selected = selectedUser === user.username
+      return (
+        <div
+          className={classNames('flex p-3 cursor-pointer hover:bg-gray-200', {
+            'bg-white': selected,
+          })}
+          key={user.username}
+          onClick={() => setSelectedUser(user.username)}
+        >
+          <img
+            src={user.imageUrl}
+            alt="user"
+            className="object-cover w-16 h-16 mr-2 rounded-full"
+          />
+          <div>
+            <p className="text-green-400">{user.username}</p>
+            <p className="font-light">
+              {user.latestMessage
+                ? user.latestMessage.content
+                : 'You are now connected!'}
+            </p>
+          </div>
         </div>
-      </div>
-    ))
+      )
+    })
   }
 
   return <div className="bg-gray-100">{usersMarkup}</div>
