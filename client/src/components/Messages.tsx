7ae9baaad833bc799f0c789d5e7c99dfcb568d28
ChandrakantPal <sanjay161097@gmail.com>
2021-04-05 18:45:31 +0530

    message box component added with other styling

--- client/src/components/Messages.tsx
+++ client/src/components/Messages.tsx
@@ -1,7 +1,8 @@
 import { gql, useLazyQuery } from '@apollo/client'
 import { useEffect } from 'react'
-import { useMessageDispatch, useMessageState } from '../context/Message'
+import { useMessageDispatch, useMessageState } from '../context/MessageContext'
 import { Message } from '../types'
+import MessageBox from './Message'
 
 const Messages = () => {
   const { selectedUser, messages } = useMessageState()
@@ -32,14 +33,25 @@ const Messages = () => {
   } else if (loading) {
     selectedChat = <p>Loading...</p>
   } else if (messages.length > 0) {
-    selectedChat = messages.map((message: Message) => (
-      <p key={message.uuid}>{message.content}</p>
+    selectedChat = messages.map((message: Message, index) => (
+      <>
+        <MessageBox message={message} key={message.uuid} />
+        {index === messages.length - 1 && (
+          <div className="invisible">
+            <hr className="m-0" />
+          </div>
+        )}
+      </>
     ))
   } else if (messages.length === 0) {
     selectedChat = <p>You are now connected! send your first message!</p>
   }
 
-  return <div className="col-span-2">{selectedChat}</div>
+  return (
+    <div className="flex flex-col-reverse col-span-2 overflow-y-scroll h-96 no-scrollbar">
+      {selectedChat}
+    </div>
+  )
 }
 
 export default Messages
