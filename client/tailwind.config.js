31dc8d0f6988233d7fb6254dcbb413f2a97a087e
ChandrakantPal <sanjay161097@gmail.com>
2021-04-06 22:52:27 +0530

    160 spacing added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        70: '17.5rem',
+        160: '40rem',
+      },
+    },
   },
   variants: {
     extend: {},
