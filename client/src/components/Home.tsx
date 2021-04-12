748b6ee746b38ca99a74243afcb5227c9925c9b0
ChandrakantPal <sanjay161097@gmail.com>
2021-04-12 19:18:44 +0530

    new message subscription

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -3,9 +3,30 @@ import { useAuthState } from '../context/Auth'
 import Navbar from './Navbar'
 import Users from './Users'
 import Messages from './Messages'
+import { gql, useSubscription } from '@apollo/client'
+import { useEffect } from 'react'
+import { useMessageDispatch, useMessageState } from '../context/MessageContext'
 
 const Home = () => {
   const { authenticated } = useAuthState()
+  const dispatch = useMessageDispatch()
+  const { selectedUser } = useMessageState()
+
+  const { data, error: messageError } = useSubscription(NEW_MESSAGE)
+
+  console.log({ data })
+
+  useEffect(() => {
+    if (messageError) console.log(messageError)
+    if (
+      data &&
+      (selectedUser === data.newMessage.from ||
+        selectedUser === data.newMessage.to)
+    ) {
+      console.log({ data })
+      dispatch('ADD_MESSAGE', data.newMessage)
+    }
+  }, [data, messageError, selectedUser])
 
   if (!authenticated) return <Redirect to="/login" />
   return (
@@ -22,3 +43,15 @@ const Home = () => {
 }
 
 export default Home
+
+const NEW_MESSAGE = gql`
+  subscription newMessage {
+    newMessage {
+      uuid
+      content
+      from
+      to
+      createdAt
+    }
+  }
+`
