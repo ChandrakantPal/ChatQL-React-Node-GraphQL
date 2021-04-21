54ae75960557ddaa3060a7da8bf1c0e12c2a7b34
ChandrakantPal <sanjay161097@gmail.com>
2021-04-21 21:34:26 +0530

    reaction buton markup

---
+++ client/src/components/Reaction.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Reaction = () => {
+  return (
+    <button className="px-2 outline-none focus:outline-none">
+      <i className="far fa-smile"></i>
+    </button>
+  )
+}
+
+export default Reaction
