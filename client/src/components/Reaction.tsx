eb79a6089a5975bf48b885af94f23f4152022a55
ChandrakantPal <sanjay161097@gmail.com>
2021-04-22 22:41:11 +0530

    show popup when clicked on smilie button

--- client/src/components/Reaction.tsx
+++ client/src/components/Reaction.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Reaction = () => {
+  const [showPopup, setShowPopup] = useState(false)
+
   return (
-    <button className="px-2 outline-none focus:outline-none">
-      <i className="far fa-smile"></i>
-    </button>
+    <>
+      <button
+        className="px-2 outline-none focus:outline-none"
+        onClick={() => setShowPopup((prevState) => !prevState)}
+      >
+        <i className="far fa-smile"></i>
+      </button>
+      {showPopup && (
+        <p className="px-3 py-2 mx-1 text-sm text-gray-900 bg-gray-100 bg-opacity-50 rounded-lg shadow">
+          Reaction
+        </p>
+      )}
+    </>
   )
 }
 
