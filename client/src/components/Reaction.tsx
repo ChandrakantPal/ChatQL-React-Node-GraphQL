82ecfc09c1a31f0f4d10db940cc46fbfe71eb3d7
ChandrakantPal <sanjay161097@gmail.com>
2021-04-23 22:26:40 +0530

    react to message mutation

--- client/src/components/Reaction.tsx
+++ client/src/components/Reaction.tsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import { gql, useMutation } from '@apollo/client'
+import { FC, useState } from 'react'
 
-const Reaction = () => {
+interface ReactionProp {
+  uuid: string
+}
+
+const Reaction: FC<ReactionProp> = ({ uuid }) => {
   const [showPopup, setShowPopup] = useState(false)
 
-  const react = (reaction: string) => {
-    setShowPopup(false)
+  const [reactToMessage] = useMutation(REACT_TO_MESSAGE, {
+    onError: (err) => console.log(err),
+    onCompleted: (data) => {
+      console.log(data)
+      setShowPopup(false)
+    },
+  })
+
+  const react = (content: string) => {
+    reactToMessage({ variables: { uuid, content } })
   }
 
   const reactions = ['❤️', '😆', '😯', '😢', '😡', '👍', '👎']
@@ -17,7 +30,7 @@ const Reaction = () => {
         <i className="far fa-smile"></i>
       </button>
       {showPopup && (
-        <div className="px-3 py-2 mx-1 text-sm text-gray-900 bg-gray-100 bg-opacity-50 rounded-lg shadow">
+        <div className="px-3 py-2 mx-1 text-sm text-gray-900 bg-gray-100 bg-opacity-50 rounded-lg shadow ">
           {reactions.map((reaction) => (
             <button
               className="mr-1 transition duration-500 ease-in-out transform outline-none focus:outline-none hover:-translate-y-1 hover:scale-150"
@@ -34,3 +47,11 @@ const Reaction = () => {
 }
 
 export default Reaction
+
+const REACT_TO_MESSAGE = gql`
+  mutation reactToMessage($uuid: String!, $content: String!) {
+    reactToMessage(uuid: $uuid, content: $content) {
+      uuid
+    }
+  }
+`
